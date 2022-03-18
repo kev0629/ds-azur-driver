0d41c13cf2325fbae48ddf4071edfaf68c150d04
kevin saffioti <kevin.saffioti@gmail.com>
2022-03-18 17:29:00 +0100

    fix hhtps;// issue on facebook link

--- Component/Footer.jsx
+++ Component/Footer.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
                     Adresse : 30 traverse des Cades 06370 Mouans Sartoux <br />
                     Tel : <a href="tel:+330608070873">06.08.07.08.73</a> <br />
                     Email :  <a href="david.saffioti@dsazurdriver.fr">david.saffioti@dsazurdriver.fr</a> <br />
-                    <Link href="https://https://www.facebook.com/DS-Azur-Driver-107376328499902/" passHref={true}><BsFacebook className='icon'/></Link>
+                    <Link href="https://www.facebook.com/DS-Azur-Driver-107376328499902/" passHref={true}><BsFacebook className='icon'/></Link>
                     <Link href="https://instagram.com/dsazurdriver?utm_medium=copy_link" passHref={true}><BsInstagram className='icon'/></Link>
                     <Link href={"https://l.facebook.com/l.php?u=https%3A%2F%2Fapi.whatsapp.com%2Fsend%3Fphone%3D33608070873%26app%3Dfacebook%26entry_point%3Dpage_cta%26fbclid%3DIwAR35-Q4Ns5wXOtcvrtRQQoIi6F7cHGxmsDiHz3yIgiHxrXt06B7UMG_Z-QI&h=AT1LBqBfSMXB15Zs4GaJeW49jc035cRduW_K4D9BCQJ0q_yO1rJ7iKLeeMwgJhkOCMvXyiRemNOzXdt9vizH8HQzBjkGa6wrYXoTLhwUDq0JSJ_RGFjNp6mpRQNOXF-R8XGoUIA3soFoxO0"} passHref={true}> <BsWhatsapp className='icon'/></Link>
                     <Link href="https://www.linkedin.com/in/david-saffioti-236a35b1/" passHref={true}> <BsLinkedin className='icon'/></Link>
